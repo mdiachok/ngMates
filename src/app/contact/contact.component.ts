7f0847c1cb9a97e6e239f6ce468b63db4a9a2089
VolodymyrKmet <kmetvo@gmail.com>
2017-09-12 13:06:19 +0000

    Resolve "footer"

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {Observable} from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import {NgForm} from "@angular/forms";
 
 @Component({
   moduleId: module.id,
@@ -8,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
+  }
+
+  Send(desc: {}) {
+    this.items.push({ message: desc});
+    this.msgVal = '';
+  }
+  onSubmit(form:NgForm) {
+    this.Send(form.value)
+  }
 
   ngOnInit() {
   }
-
 }
