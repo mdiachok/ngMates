9d5c26333736be25d9a138e901c4ef61704f8d53
Yevheniy Potupa <y.potupa@gmail.com>
2017-09-18 17:19:51 +0300

    universal_w_out_firbase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {Observable} from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
+// import * as firebase from 'firebase/app';
 import {NgForm} from "@angular/forms";
 
 @Component({
@@ -15,28 +15,29 @@ export class HomeComponent implements OnInit {
   mainImg: string;
   mainVideo: string;
 
-  user: Observable<firebase.User>;
-  items: FirebaseListObservable<any[]>;
+  // user: Observable<firebase.User>;
+  // items: FirebaseListObservable<any[]>;
   msgVal: string = '';
 
-  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+  // constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+  constructor() {
     this.mainImg = '../assets/images/main-img.jpg',
-    this.mainVideo = '../assets/media/office.mp4',
+    this.mainVideo = '../assets/media/office.mp4'
 
-    this.items = af.list('/messages', {
-      query: {
-        limitToLast: 50
-      }
-    });
+    // this.items = af.list('/messages', {
+    //   query: {
+    //     limitToLast: 50
+    //   }
+    // });
   }
 
-  Send(desc: {}) {
-    this.items.push({ message: desc});
-    this.msgVal = '';
-  }
-  onSubmit(form:NgForm) {
-    this.Send(form.value);
-  }
+  // Send(desc: {}) {
+  //   this.items.push({ message: desc});
+  //   this.msgVal = '';
+  // }
+  // onSubmit(form:NgForm) {
+  //   this.Send(form.value);
+  // }
 
   ngOnInit() {
   }
