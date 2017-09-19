c4aaf7b43f59555f20ddbc54e0da37bcfc49981a
Yevheniy Potupa <y.potupa@gmail.com>
2017-09-19 15:17:47 +0300

    maryana final changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpModule } from "@angular/http";
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
 import { Footer } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
@@ -33,10 +30,7 @@ export const firebaseConfig = {
     FormsModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule,
-    AppRoutingModule,
-    HttpModule
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
