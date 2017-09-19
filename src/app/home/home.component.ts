81118acf791faaa8086f32394866f5716106276a
Yevheniy Potupa <y.potupa@gmail.com>
2017-09-19 15:12:38 +0300

    home.ts changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
-import {AngularFireAuth} from 'angularfire2/auth';
-import {Observable} from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
-import {NgForm} from "@angular/forms";
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -15,30 +12,26 @@ export class HomeComponent implements OnInit {
   mainImg: string;
   mainVideo: string;
 
-  user: Observable<firebase.User>;
   items: FirebaseListObservable<any[]>;
-  msgVal: string = '';
+  msgVal = '';
 
-  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
-    this.mainImg = '../assets/images/main-img.jpg',
-      this.mainVideo = '../assets/media/office.mp4',
+  constructor(public af: AngularFireDatabase) {
+    this.mainImg = '../assets/images/main-img.jpg';
+    this.mainVideo = '../assets/media/office.mp4';
 
-      this.items = af.list('/messages', {
-        query: {
-          limitToLast: 50
-        }
-      });
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
   }
 
   Send(desc: {}) {
-    this.items.push({message: desc});
+    this.items.push({ message: desc});
     this.msgVal = '';
   }
-
   onSubmit(form: NgForm) {
     this.Send(form.value);
-    form.reset();
-    console.log(name)
   }
 
   ngOnInit() {
